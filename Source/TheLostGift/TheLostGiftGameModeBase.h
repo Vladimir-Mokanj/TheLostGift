93b9a6c21ffe02b6a1da6563d3653ddd4bc14366
Vladimir <vlada.mokanj@gmail.com>
2023-05-18 17:18:42 +0200

    Initial commit

---
+++ Source/TheLostGift/TheLostGiftGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheLostGiftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELOSTGIFT_API ATheLostGiftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
