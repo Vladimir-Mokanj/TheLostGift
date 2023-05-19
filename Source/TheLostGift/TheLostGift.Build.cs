e17b6db4be32b864b1bf7b76ec49a5f73882ffc5
Vladimir <vlada.mokanj@gmail.com>
2023-05-19 23:58:52 +0200

    Added character controller
    It controlls the inputs of the character. Character itself will do the movement.

--- Source/TheLostGift/TheLostGift.Build.cs
+++ Source/TheLostGift/TheLostGift.Build.cs
@@ -10,7 +10,7 @@ public class TheLostGift : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Kismet", "BlueprintGraph", "UnrealEd" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "HTTP" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "HTTP" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
